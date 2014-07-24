2728173bfe41599f9def80f91bb7685ade1584ff
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-07-24 21:10:33 +0000

    small figures
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/opfp@19 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- benchmark/figure-systemtime-arrays.R
+++ benchmark/figure-systemtime-arrays.R
@@ -84,6 +84,69 @@ pdf("figure-systemtime-arrays-fpop-pelt.pdf", w=10)
 print(scatter)
 dev.off()
 
+faster.labels <-
+  data.frame(x=c(-0.5, -1.6), y=c(-1, -1),
+             label=c(sprintf("fpop faster %d problems",
+               faster.counts$fpop.faster),
+               sprintf("pelt faster %d problems",
+                       faster.counts$pelt.faster)))
+inaccurate <-
+  data.frame(x=0.013, y=0.006,
+             label=paste("system.time inaccurate for   ",
+               length(pid.chr.pelt.fast), "problems"))
+inacc.rects <-
+  data.frame(xmin=0, ymin=0, xmax=0.01, ymax=0.01)
+both.refs <-
+  rbind(data.frame(refs, competitor="pelt"),
+        data.frame(small.refs, competitor="binseg"))
+binseg.inacc <- wide %.%
+  filter(fpop.SIC < 0.01,
+         multiBinSeg.52 < 0.01)
+scatter <-
+  ggplot()+
+  geom_rect(aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax),
+            fill="violet", alpha=1/2, data=inacc.rects)+
+  geom_abline(aes(slope=slope, intercept=intercept),
+              data=data.frame(slope=1, intercept=0))+
+  geom_hline(aes(yintercept=seconds),
+             data=small.refs, color=ref.color)+
+  geom_vline(aes(xintercept=seconds),
+             data=both.refs,
+             color=ref.color)+
+  geom_text(aes(seconds, 10^-0.5, label=unit),
+             data=both.refs, color=ref.color, angle=90, vjust=-0.5, size=4)+
+  geom_text(aes(1e-2, seconds, label=unit),
+            data=small.refs, vjust=1.5, hjust=0, color=ref.color, size=4)+
+  geom_point(aes(pelt.SIC, fpop.SIC),
+             data=data.frame(wide, competitor="pelt"),
+             pch=1)+
+  geom_point(aes(multiBinSeg.52, fpop.SIC),
+             data=data.frame(wide, competitor="binseg"),
+             pch=1)+
+  facet_grid(. ~ competitor, labeller=function(var, val){
+    paste0("competitor = ", val)
+  }, scales="free", space="free_x")+
+  scale_x_log10("competitor (seconds)",
+                minor_breaks=NULL,
+                breaks=10^seq(-2, 1, by=1))+
+  scale_y_log10("fpop (seconds)",
+                minor_breaks=NULL,
+                breaks=10^seq(-2, 0, by=1))+
+  geom_text(aes(x, y, label=label),
+            data=data.frame(inaccurate, competitor="pelt"),
+            color="violet", hjust=0, size=3)+
+  geom_text(aes(x, y, label=label),
+            data=data.frame(x=0.0115, y=0.006,
+              competitor="binseg",
+              label=paste("system.time inaccurate\nfor",
+                nrow(binseg.inacc), "problems")),
+            color="violet", hjust=0, size=3)+
+  theme_grey()
+
+pdf("figure-systemtime-arrays-fpop-pelt-small.pdf", w=7, h=3)
+print(scatter)
+dev.off()#;system("evince figure-systemtime-arrays-fpop-pelt-small.pdf")
+
 binseg.counts <- wide %.%
   summarise(fpop.faster=sum(multiBinSeg.52 > fpop.SIC),
             same.speed=sum(multiBinSeg.52 == fpop.SIC),
@@ -98,9 +161,6 @@ binseg.labels <- with(binseg.counts, {
                sprintf("binary segmentation=fpop\n%d problems",
                        same.speed)))
 })
-binseg.inacc <- wide %.%
-  filter(fpop.SIC < 0.01,
-         multiBinSeg.52 < 0.01)
 scatter2 <-
   ggplot()+
   geom_rect(aes(xmin=0, ymin=0,
@@ -132,6 +192,36 @@ scatter2 <-
 pdf("figure-systemtime-arrays-fpop-multiBinSeg.pdf")
 print(scatter2)
 dev.off()
+
+with.leg <-
+  ggplot()+
+  geom_hline(aes(yintercept=seconds), data=refs)+
+  geom_text(aes(25, seconds, label=unit),
+            data=refs, vjust=1.5, hjust=0)+
+  geom_point(aes(probes, seconds, color=abbrev),
+             data=timings, pch=1)+
+  scale_shape_manual(values=c(one=1, several=19))+
+  scale_color_manual(values=algo.colors)+
+  theme_grey()+
+  scale_x_log10("number of data points to segment",
+                limits=10^c(1.3, 5.9),
+                minor_breaks=NULL,
+                breaks=c(10^seq(2, 4, by=1), range(timings$probes)))+
+  scale_y_log10("seconds",labels=function(x)sprintf("%.2f", x),
+                minor_breaks=NULL,
+                breaks=10^seq(-2, 4, by=1))
+pos.method <-
+  list("last.points",
+       "calc.boxes",
+       dl.trans(h=h*3/2, x=x+0.1),
+       "calc.borders",
+       ##"draw.rects",
+       qp.labels("y","bottom","top",make.tiebreaker("x","y"),ylimits))
+dl <- direct.label(with.leg, pos.method)
+
+pdf("figure-systemtime-arrays-small.pdf", w=4, h=3)
+print(dl)
+dev.off()#;system("evince figure-systemtime-arrays-small.pdf")
 
 with.leg <-
   ggplot()+

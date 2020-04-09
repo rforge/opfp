c264029f9a9de82f8475955408257b513846f2d8
tdhock <tdhock@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2020-04-09 20:00:34 +0000

    delete pkg_cran
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/opfp@65 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg_cran/R/onLoad.R
+++
@@ -1,5 +0,0 @@
-.onAttach <- function(lib, pkg, ...) {
-  packageStartupMessage("Welcome to the fpop package.
-This package implements the FPOP algorithm (http://arxiv.org/abs/1409.1842),
-see the Fpop function.")
-}
--- pkg_cran/src/Call_BinSeg_MultiDim.h
+++
@@ -1,9 +0,0 @@
-
-#include "BinSeg_MultiDim.h"
-
-
-//extern "C" {  // new 31.01.2019 for binseg
-    void Call_BinSeg (double * dataVec_, int * Kmax_, int * n_, int * P_, int * Ruptures_,  double * RupturesCost_);
-//}  // new 31.01.2019 for binseg
-
-
